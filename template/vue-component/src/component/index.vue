1d7f80247daec5f2d1b68220cf6f7fa93a66b0b3
liangren.wu <liangren.wu@ipptravel.com>
2018-04-02 16:07:25 +0800

    优化

---
+++ template/vue-component/src/component/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="test-component">
+    <div class="box">
+      <span class="prev" @click="reduce">减少</span>
+      <span>{{ count }}</span>
+      <span class="next" @click="add">增加</span>
+    </div>
+    <p>{{ msg }}</p>
+    <p><span @click="callback(count)">向父组件传值</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    msg() {
+      return `加加减减最终为 ${this.count} ；`;
+    }
+  },
+  props: {
+    callback: {
+      type: Function
+    }
+  },
+  methods: {
+    add() {
+      this.count = this.count + 1;
+    },
+    reduce() {
+      this.count = this.count - 1;
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @import './index.less';
+</style>
