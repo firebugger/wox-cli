bce3b2de55fbe0a53df45d59d7513681fd094864
liangren.wu <liangren.wu@ipptravel.com>
2018-04-03 16:29:59 +0800

    修复禁止master编译导致非GIT项目报错问题

--- template/toolkit/vue-multi/publicPath.config.js
+++ template/toolkit/vue-multi/publicPath.config.js
@@ -2,9 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const currentDir = __dirname.substr(__dirname.lastIndexOf('/') + 1);
-let gitVersion = fs.readFileSync(path.resolve(__dirname, './.git/HEAD'), 'utf8');
-gitVersion = gitVersion.split('/heads/')[1].replace(/\s/g, '');
-const versionNum = gitVersion.split('/')[1];
+let gitVersion = 'daily/0.0.1';
+let versionNum = '0.0.1';
+
+try {
+  const gitHead = fs.readFileSync(path.resolve(__dirname, './.git/HEAD'), 'utf8');
+  gitVersion = gitHead.split('/heads/')[1].replace(/\s/g, '');
+  versionNum = gitVersion.split('/')[1];
+} catch (e) { 
+  console.log('not a git project!');
+}
 
 const publicPath = `//www.quimg.com/${currentDir}/${versionNum}/`;
 
